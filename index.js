7369d246e510f09b20256601d33590242990ab47
Dev-exponent1 <18-52hl100@students.unilorin.edu.ng>
2021-12-14 13:16:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const SI=(P,R,T)=>{
+    interest=(P*R*T)/100;
+}
+let SII=SI(2000,2.5,3)
+console.log(interest);
+function myScore(a,b){
+    score=a*b
+}
+var techilorin={digprom:{kidtech:["adult", "child"], femtech:"book"}}
+console.log(techilorin.digprom.kidtech[0])
+function getNumber(){
+    let randomNumb= document.getElementById("random")
+    randomNumb.value=Math.floor(1000 + Math.random() * 9000000000000000);
+}
